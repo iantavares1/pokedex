da67c7381661bff1d0a5cf9a090e8429282266dc
Ian Tavares <iantavares2405@gmail.com>
2023-06-17 16:31:12 -0300

    add FavContext to PokeInfo

--- src/components/PokeInfo/index.jsx
+++ src/components/PokeInfo/index.jsx
@@ -1,6 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 
 import * as S from './styles'
+
+import { FavContext } from '../../contexts/favContext'
 
 import { bgColors } from '../../utils/bgColors'
 import { BackIcon } from '../common/BackIcon'
@@ -8,17 +10,36 @@ import { formatString } from '../../utils/formatString'
 import { fetchPokemon } from '../../api/fetchPokemon'
 import { formatId } from '../../utils/formatId'
 import { typeIcons } from '../../utils/typeIcons'
+import { HeartIcon } from '../common/HeartIcon'
 
 export const PokeInfo = ({ info, isOpen }) => {
-  const { name, id, type, type2, imgUrl, imgUrl2 } = info
-  const [pokemon, setPokemon] = useState({})
+  const favoritesContext = useContext(FavContext)
 
+  const { name, id, type, type2, imgUrl, imgUrl2 } = info
   const TypeIcon = typeIcons[type]
   const TypeIcon2 = typeIcons[type2]
+
+  const [pokemon, setPokemon] = useState({})
+  const [isFavorite, setIsFavorite] = useState(false)
+  const [detailsPage, setDetailsPage] = useState('about')
+
+  const loadIsFavorite = () =>
+    setIsFavorite(!!favoritesContext.favorites.includes(name))
+
+  const handleIsFavorite = () => {
+    favoritesContext.updateFavorites(name)
+    loadIsFavorite()
+  }
 
   const handleClosePokeInfo = () => {
     isOpen(false)
   }
+
+  const handleDetailsPage = (page) => setDetailsPage(page)
+
+  useEffect(() => {
+    loadIsFavorite()
+  })
 
   useEffect(() => {
     const getPokemon = async () => {
@@ -26,45 +47,91 @@ export const PokeInfo = ({ info, isOpen }) => {
         const response = await fetchPokemon(name)
         setPokemon(response)
       } catch (error) {
-        console.log(`error: ${error}`)
+        console.log(`Erro: ${error}`)
       }
     }
     getPokemon()
   }, [isOpen, name])
 
   return (
-    <>
-      {info ? (
-        <S.Container bg={bgColors[type]}>
-          <S.Button onClick={handleClosePokeInfo}>
-            <BackIcon />
-          </S.Button>
-          <TypeIcon />
+    <S.Container bg={bgColors[type]}>
+      <div className="top-buttons">
+        <S.Button onClick={handleClosePokeInfo}>
+          <BackIcon fill={'white'} />
+        </S.Button>
 
-          <S.Main>
-            <div className="top">
-              <h1>{formatString(name)}</h1>
-              <span>{formatId(id)}</span>
+        <S.Button onClick={handleIsFavorite}>
+          <HeartIcon fill={isFavorite && 'white'} />
+        </S.Button>
+      </div>
+      <TypeIcon />
+
+      <S.Main>
+        <div className="top">
+          <h1>{formatString(name)}</h1>
+          <span>{formatId(id)}</span>
+        </div>
+        <div className="types-wrapper">
+          <S.Type bg={bgColors[type]}>
+            <TypeIcon />
+            {formatString(type)}
+          </S.Type>
+          {type2 && (
+            <S.Type bg={bgColors[type2]}>
+              <TypeIcon2 />
+              {formatString(type2)}
+            </S.Type>
+          )}
+        </div>
+        <S.Img src={imgUrl || imgUrl2} />
+      </S.Main>
+      <S.DetailsWrapper>
+        <header>
+          <span className="buttons">
+            <div className="button-wrapper">
+              <S.DetailButton
+                active={detailsPage === 'about'}
+                onClick={() => handleDetailsPage('about')}
+              >
+                About
+              </S.DetailButton>
             </div>
-            <div className="types-wrapper">
-              <S.Type bg={bgColors[type]}>
-                <TypeIcon />
-                {formatString(type)}
-              </S.Type>
-              {type2 && (
-                <S.Type bg={bgColors[type2]}>
-                  <TypeIcon2 />
-                  {formatString(type2)}
-                </S.Type>
-              )}
+            <div className="button-wrapper">
+              <S.DetailButton
+                active={detailsPage === 'stats'}
+                onClick={() => handleDetailsPage('stats')}
+              >
+                Stats
+              </S.DetailButton>
             </div>
-            <S.Img src={imgUrl || imgUrl2} />
-          </S.Main>
-          <S.Details>{pokemon && ''}</S.Details>
-        </S.Container>
-      ) : (
-        ''
-      )}
-    </>
+            <div className="button-wrapper">
+              <S.DetailButton
+                active={detailsPage === 'evolution'}
+                onClick={() => handleDetailsPage('evolution')}
+              >
+                Evolution
+              </S.DetailButton>
+            </div>
+          </span>
+        </header>
+        {detailsPage === 'about' && (
+          <S.Details>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <span className="metrics">
+              <div className="height">
+                <span>Height</span>
+                <span>{pokemon.height / 10}m</span>
+              </div>
+              <div className="weight">
+                <span>Weight</span>
+                <span>{pokemon.weight / 10}Kg</span>
+              </div>
+            </span>
+          </S.Details>
+        )}
+        {detailsPage === 'stats' && <S.Details>oi</S.Details>}
+        {detailsPage === 'evolution' && <S.Details>Seila</S.Details>}
+      </S.DetailsWrapper>
+    </S.Container>
   )
 }
--- src/components/PokeInfo/styles.js
+++ src/components/PokeInfo/styles.js
@@ -8,18 +8,21 @@ export const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background: ${({ bg }) => bg};
-  color: ${({ theme }) => theme.text.primary};
   animation: get-in 0.2s linear;
 
-  > :nth-child(1) {
-    margin: 2rem 0 0 2rem;
+  .top-buttons {
+    margin: 2rem;
+    display: flex;
+    justify-content: space-between;
   }
+
   > :nth-child(2) {
     position: absolute;
     z-index: -1;
     top: -2rem;
     left: -2rem;
-    opacity: 0.2;
+    color: white;
+    opacity: 0.1;
     width: 40rem;
     height: 40rem;
     pointer-events: none;
@@ -31,14 +34,6 @@ export const Container = styled.div`
     }
     to {
       right: 0;
-    }
-  }
-  @keyframes get-out {
-    from {
-      right: 0;
-    }
-    to {
-      right: -100vw;
     }
   }
 `
@@ -53,6 +48,7 @@ export const Main = styled.div`
   padding: 1rem 2rem;
   height: 45vh;
   display: flex;
+  color: white;
   flex-direction: column;
   align-items: flex-start;
   gap: 0.5rem;
@@ -68,7 +64,7 @@ export const Main = styled.div`
     }
 
     span {
-      font-size: 1.8;
+      font-size: 1.8rem;
       font-weight: 600;
     }
   }
@@ -96,20 +92,82 @@ export const Type = styled.div`
   display: flex;
   gap: 0.3rem;
   align-items: center;
-  font-size: 14px;
+  font-size: 1.4rem;
 `
 
-export const Details = styled.div`
+export const DetailsWrapper = styled.div`
   width: 100%;
   height: 100%;
+  padding: 3rem;
+
   background: ${({ theme }) => theme.bg.primary};
+  color: ${({ theme }) => theme.text.primary};
+
   display: flex;
   flex-direction: column;
   border-radius: 3rem;
 
-  > button {
-    all: unset;
+  .buttons {
+    border: solid 0.1rem red;
+
+    display: flex;
+
+    .button-wrapper {
+      position: relative;
+      width: calc(100% / 3);
+      padding-bottom: 0.7rem;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .bar {
+        position: absolute;
+        bottom: 0;
+
+        width: 3rem;
+        height: 0.4rem;
+        border-radius: 0.2rem;
+        background: ${({ theme }) => theme.text.primary};
+      }
+    }
   }
 `
 
-export const Detail = styled.div``
+export const DetailButton = styled.button`
+  all: unset;
+  color: ${({ theme }) => theme.text.primary};
+  opacity: 0.8;
+  position: relative;
+  line-height: 2.4rem;
+
+  ${({ active }) =>
+    active &&
+    `
+    font-weight: bold;
+    border-bottom: .5rem solid;
+  `}
+`
+
+export const Details = styled.div`
+  padding: 2rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+
+  .metrics {
+    padding: 2rem 4rem;
+    background-color: ${({ theme }) => theme.bg.primary};
+    box-shadow: 1rem 1rem 0.7rem -0.7rem rgba(0, 0, 0, 0.52);
+    border-radius: 1.3rem;
+
+    display: flex;
+    gap: 4rem;
+
+    div {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+`
