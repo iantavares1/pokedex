9c2a1086b6411744d4ccd4309ba04c93423135b2
Ian Tavares <iantavares2405@gmail.com>
2023-06-26 20:46:04 -0300

    fix slider

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -2,21 +2,27 @@ import * as S from './styles'
 
 import { bgColors } from '../../utils/bgColors'
 import { formatString } from '../../utils/formatString'
+import { NextPreviousIcon } from '../common/NextPrevious'
 
 export const Slider = ({ onChoice }) => {
-  const getType = (e) => onChoice(e.target.innerHTML)
+  const getType = (e) => {
+    onChoice(e.target.innerHTML)
+    console.log(e.target.innerHTML)
+  }
 
   return (
     <S.Container>
+      <NextPreviousIcon />
       {Object.keys(bgColors).map((type) => (
         <S.TypeButton
           key={type}
           style={{ background: `${bgColors[type]}` }}
           onClick={getType}
         >
-          <span>{formatString(type)}</span>
+          {formatString(type)}
         </S.TypeButton>
       ))}
+      <NextPreviousIcon />
     </S.Container>
   )
 }
--- src/components/Slider/styles.js
+++ src/components/Slider/styles.js
@@ -4,24 +4,12 @@ export const Container = styled.div`
   display: flex;
   gap: 0.8rem;
   overflow-x: scroll;
-
-  &::-webkit-scrollbar {
-    height: 1rem;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    background: ${({ theme }) => theme.scrollbar.primary};
-    border-radius: 0.5rem;
-  }
 `
 
 export const TypeButton = styled.button`
   all: unset;
   padding: 0.2rem 1.5rem;
   border-radius: 1.3rem;
+  color: white;
   cursor: pointer;
-
-  > span {
-    color: white;
-  }
 `
