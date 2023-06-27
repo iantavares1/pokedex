d1073f449806e68ebd07c4455202b80840add8fa
Ian Tavares <iantavares2405@gmail.com>
2023-06-26 21:26:55 -0300

    add next and previous buttons to slider

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react'
+
 import * as S from './styles'
 
 import { bgColors } from '../../utils/bgColors'
@@ -5,24 +7,44 @@ import { formatString } from '../../utils/formatString'
 import { NextPreviousIcon } from '../common/NextPrevious'
 
 export const Slider = ({ onChoice }) => {
+  const sliderRef = useRef(null)
+
   const getType = (e) => {
     onChoice(e.target.innerHTML)
     console.log(e.target.innerHTML)
   }
 
+  const handleSlider = (direction) => {
+    const slider = sliderRef.current
+
+    if (slider) {
+      const width = 300
+      const scrollAmount = direction === 'next' ? width : -width
+      slider.scrollLeft += scrollAmount
+    }
+  }
+
   return (
     <S.Container>
-      <NextPreviousIcon />
-      {Object.keys(bgColors).map((type) => (
-        <S.TypeButton
-          key={type}
-          style={{ background: `${bgColors[type]}` }}
-          onClick={getType}
-        >
-          {formatString(type)}
-        </S.TypeButton>
-      ))}
-      <NextPreviousIcon />
+      <S.Button onClick={() => handleSlider('previous')}>
+        <NextPreviousIcon direction="l" />
+      </S.Button>
+
+      <S.Slider ref={sliderRef}>
+        {Object.keys(bgColors).map((type) => (
+          <S.TypeButton
+            key={type}
+            style={{ background: `${bgColors[type]}` }}
+            onClick={getType}
+          >
+            {formatString(type)}
+          </S.TypeButton>
+        ))}
+      </S.Slider>
+
+      <S.Button onClick={() => handleSlider('next')}>
+        <NextPreviousIcon direction="r" />
+      </S.Button>
     </S.Container>
   )
 }
--- src/components/Slider/styles.js
+++ src/components/Slider/styles.js
@@ -2,14 +2,42 @@ import { styled } from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
+  gap: 1.5rem;
+  align-items: center;
+`
+
+export const Slider = styled.div`
+  display: flex;
+  align-items: center;
   gap: 0.8rem;
   overflow-x: scroll;
+  scroll-behavior: smooth;
+
+  @media (min-width: 768px) {
+    overflow: hidden;
+  }
 `
 
 export const TypeButton = styled.button`
   all: unset;
+  height: 2rem;
   padding: 0.2rem 1.5rem;
   border-radius: 1.3rem;
   color: white;
   cursor: pointer;
+`
+
+export const Button = styled.button`
+  all: unset;
+  padding-bottom: 0.15rem;
+  height: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  display: none;
+
+  @media (min-width: 768px) {
+    display: block;
+  }
 `
