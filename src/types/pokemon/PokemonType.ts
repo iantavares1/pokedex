d41a9b12a4c5e60967614ccdbefcbe7d1d037868
Ian Tavares <iantavares2405@gmail.com>
2023-08-01 18:00:18 -0300

    add types

---
+++ src/types/pokemon/PokemonType.ts
@@ -0,0 +1,21 @@
+type PokemonType =
+  | 'normal'
+  | 'fire'
+  | 'water'
+  | 'electric'
+  | 'grass'
+  | 'ice'
+  | 'flying'
+  | 'fighting'
+  | 'fairy'
+  | 'poison'
+  | 'ground'
+  | 'psychic'
+  | 'bug'
+  | 'rock'
+  | 'ghost'
+  | 'dragon'
+  | 'dark'
+  | 'steel'
+
+export default PokemonType
