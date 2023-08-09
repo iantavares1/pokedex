97306de9bb39ca99ea1e3803db969fe5b5023ae6
Ian Tavares <iantavares2405@gmail.com>
2023-08-09 14:01:03 -0300

    add React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
+
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './services/queryClient'
+
 import { ThemeProvider } from '@emotion/react'
 import { CssBaseline } from '@mui/material'
 import theme from './theme'
+
 import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
